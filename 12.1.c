19bf055eab17a9afdb851377eefcf0c999d00e19
JeongSeongGil <63443366+JeongSeongGil@users.noreply.github.com>
2021-03-16 19:51:13 +0900

    Add files via upload

---
+++ 12.1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int num1=1+2, num2=1-2;
+	
+	printf("%d\n", num1);
+	printf("%d\n", num2);
+	
+	return 0;
+	
+}
---
+++ 12.2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int num1=2, num2=1;
+	num1=num1+ 2 ;
+	num2= num2-2;
+	num1= num1+2;
+	num1=1;
+	
+	printf("num1=%d\n", num1);
+	printf("num2=%d\n", num2);
+	
+	return 0;
+}
