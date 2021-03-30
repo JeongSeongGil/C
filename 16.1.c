2f827d1e5e8f33d4020a8350d4bec60556c84554
JeongSeongGil <63443366+JeongSeongGil@users.noreply.github.com>
2021-03-30 19:45:24 +0900

    Add files via upload

---
+++ 16.1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int num1 = 11, int num2 ;
+	printf("%f\n", num1 + num2);
+	printf("%f\n", num1 - num2);
+	printf("%f\n", num1 * num2);
+	printf("%f\n", num1 / num2);
+	
+	return 0;
+}
