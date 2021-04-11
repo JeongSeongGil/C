a6dc3673c25249eb073bde40d7aac547f7941884
JeongSeongGil <63443366+JeongSeongGil@users.noreply.github.com>
2021-04-11 16:40:45 +0900

    Add files via upload

---
+++ 17.1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int num1 = 9;
+	
+	if (num1==10)
+	{
+		printf("11\n"); 
+	}
+	
+	if (num1 ==9) 
+	{
+		printf("10¿‘¥œ¥Ÿ\n");
+		printf("9\n");
+	}
+	
+	return 0;
+}
---
+++ 17.3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	int num1 =5;
+	
+	if (num1 == 5)
+		printf("20입니다\n"),	printf("10입니다\n");
+	return 0;
+}
